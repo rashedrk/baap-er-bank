d941be00883ac298492e30c2c3c3ed8e07084782
rashedrk <rashedrk001@gmail.com>
2023-02-16 13:48:15 +0600

    login section done

---
+++ js/login.js
@@ -0,0 +1,18 @@
+// step 1 : add click handler with the submit button 
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // step 2 : get the email address inside the email input field 
+    const userEmail = document.getElementById('user-email');
+    const email = userEmail.value;
+
+    // step 3: get the password inside the password field 
+    const userPassword = document.getElementById('user-password');
+    const password = userPassword.value;
+
+    // DANGER : do not verify email and password in client side 
+    // step 4 : verify email and password 
+    if (email === 'sontan@baap.com' && password === 'secret') {
+        console.log('valid user');
+    } else {
+        console.log('invalid');
+    }
+})
