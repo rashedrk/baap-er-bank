ab32d6722a83dba1932ab3fe853dea59c791f984
rashedrk <rashedrk001@gmail.com>
2023-02-16 22:03:18 +0600

    bank page added

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // DANGER : do not verify email and password in client side 
     // step 4 : verify email and password 
     if (email === 'sontan@baap.com' && password === 'secret') {
-        console.log('valid user');
+        window.location.href = '../bank.html'
     } else {
         console.log('invalid');
     }
